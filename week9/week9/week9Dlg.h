b070e772d4463b2f5d09b437c07e91b45c98b09f
Administrator <Administrator@DESKTOP-4KVCKLV>
2020-05-19 14:46:39 +0800

    week9 静态加载bmp位图

---
+++ week9/week9/week9Dlg.h
@@ -0,0 +1,34 @@
+﻿
+// week9Dlg.h: 头文件
+//
+
+#pragma once
+
+
+// Cweek9Dlg 对话框
+class Cweek9Dlg : public CDialogEx
+{
+// 构造
+public:
+	Cweek9Dlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_WEEK9_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
