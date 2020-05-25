fad23cf3eb8e32ddd6fdfb876862a9953f26be25
Administrator <Administrator@DESKTOP-4KVCKLV>
2020-05-25 16:31:25 +0800

    week9.2

---
+++ week9.2/week9.2/week9.2.h
@@ -0,0 +1,32 @@
+﻿
+// week9.2.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// Cweek92App:
+// 有关此类的实现，请参阅 week9.2.cpp
+//
+
+class Cweek92App : public CWinApp
+{
+public:
+	Cweek92App();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern Cweek92App theApp;
---
+++ week9.2/week9.2/week9.2Dlg.h
@@ -0,0 +1,37 @@
+﻿
+// week9.2Dlg.h: 头文件
+//
+
+#pragma once
+
+
+// Cweek92Dlg 对话框
+class Cweek92Dlg : public CDialogEx
+{
+// 构造
+public:
+	Cweek92Dlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_WEEK92_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedButton1();
+	void foo(CImage& img, int& sx, int& sy, int& w, int& h);
+};
